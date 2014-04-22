bcd1ffb98fde240c66c1eb0c9efc6bfc852d76f1
Marcus Smith <Marcus.Smith@partner.bmwgroup.com>
2014-04-22 19:59:51 -0400

    added quicklook support for app and xcarchive files.  quicklook plugin now gets NSDictionary from summary command from the script.  It can find the URL to the icon file in the app. fixed #28

--- QuickLookPlugin/QuickLookPlugin/GeneratePreviewForURL.m
+++ QuickLookPlugin/QuickLookPlugin/GeneratePreviewForURL.m
@@ -19,7 +19,7 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
     
     NSTask *task = [[NSTask alloc] init];
     [task setLaunchPath:@"/usr/bin/ipaHelper"];
-    [task setArguments:[NSArray arrayWithObjects:@"summary", [ipaURL path], nil]];
+    [task setArguments:[NSArray arrayWithObjects:@"summary", [ipaURL path], @"--json", @"--ql", @"--dont-clean", nil]];
     
     NSPipe *pipe = [NSPipe pipe];
     [task setStandardOutput: pipe];
@@ -35,7 +35,28 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
     NSData *data;
     data = [file readDataToEndOfFile];
     
-    NSLog(@"Data: %@", data);
+    NSDictionary *summaryDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers  error:nil];
+    
+    [[summaryDictionary copy] enumerateKeysAndObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
+        NSLog(@"key: %@, value:%@", key, obj);
+    }];
+    
+    NSURL *iconURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@/icon.png", summaryDictionary[@"AppDirectory"]]];
+    NSLog(@"iconURL: %@", iconURL);
+    
+    //Got the image!!
+    
+    NSTask *cleanTask = [[NSTask alloc] init];
+    [cleanTask setLaunchPath:@"/usr/bin/ipaHelper"];
+    [cleanTask setArguments:[NSArray arrayWithObjects:@"clean", [ipaURL path], nil]];
+    
+    pipe = [NSPipe pipe];
+    [cleanTask setStandardOutput: pipe];
+    //The magic line that keeps your log where it belongs
+    [cleanTask setStandardInput:[NSPipe pipe]];
+    
+    NSLog(@"Launching Task");
+    [cleanTask launch];
     
 //    CGContextRef cgContext = QLPreviewRequestCreateContext(preview, CGSizeMake(100, 100), NO, NULL);
 //    QLPreviewRequestFlushContext(preview, cgContext);
