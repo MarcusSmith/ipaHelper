5fffdd9ecb29d5aa8729a290edc8ba604db18209
Marcus Smith <Marcus.Smith@partner.bmwgroup.com>
2014-04-29 16:50:49 -0400

    started making the quicklook plugin use CGContexts to draw the info #27

--- QuickLookPlugin/QuickLookPlugin/GeneratePreviewForURL.m
+++ QuickLookPlugin/QuickLookPlugin/GeneratePreviewForURL.m
@@ -41,8 +41,24 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
         NSLog(@"key: %@, value:%@", key, obj);
     }];
     
-    NSURL *iconURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@/icon.png", summaryDictionary[@"AppDirectory"]]];
+    NSURL *iconURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@/Icon.png", summaryDictionary[@"AppDirectory"]]];
     NSLog(@"iconURL: %@", iconURL);
+    NSLog(@"path: %@", [iconURL path]);
+    
+    NSError* error = nil;
+//    NSData *iconData = [NSData dataWithContentsOfURL:iconURL options:NSDataReadingUncached error:&error];
+    NSData *iconData = [NSData dataWithContentsOfFile:[iconURL path] options:NSDataReadingUncached error:&error];
+    NSImage *imageForSize = [[NSImage alloc] initWithData: iconData];
+    
+    if (error) {
+        NSLog(@"Error: %@", error);
+    } else {
+        NSLog(@"Data has loaded successfully.");
+    }
+    
+    NSLog(@"icon data: %@", iconData);
+    
+//	NSImage *icon = [[NSImage alloc] initWithData: iconData];
     
     //Got the image!!
     
@@ -58,10 +74,27 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
     NSLog(@"Launching Task");
     [cleanTask launch];
     
-//    CGContextRef cgContext = QLPreviewRequestCreateContext(preview, CGSizeMake(100, 100), NO, NULL);
+    CGContextRef cgContext = QLPreviewRequestCreateContext(preview, CGSizeMake(640, 320), NO, NULL);
+    
+    if(cgContext) {
+        
+        CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData ((__bridge CFDataRef)iconData);
+        CGImageRef iconImage = CGImageCreateWithPNGDataProvider(imgDataProvider, NULL, true, kCGRenderingIntentDefault);
+        
+        CGContextDrawImage(cgContext,CGRectMake(0, 320 - imageForSize.size.height, imageForSize.size.width, imageForSize.size.width), iconImage);
+        
+        QLPreviewRequestFlushContext(preview, cgContext);
+        
+        CFRelease(cgContext);
+        CFRelease(iconImage);
+    }
+    
+//    CIImage *icon = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:MyURL]]];
+    
+    
 //    QLPreviewRequestFlushContext(preview, cgContext);
     
-    QLPreviewRequestSetDataRepresentation(preview,(__bridge CFDataRef)data,kUTTypePlainText,NULL);
+//    QLPreviewRequestSetDataRepresentation(preview,(__bridge CFDataRef)data,kUTTypePlainText,NULL);
     
     return noErr;
 }
