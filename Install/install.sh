852031064f7521282847bbb1854543533dfd8636
Marcus Smith <Marcus.Smith@partner.bmwgroup.com>
2014-05-20 11:09:18 -0400

    added resign workflow, modified xcodeproject to copy quicklook generator to install folder, modified install script to install workflow and quicklook generator

--- Install/install.sh
+++ Install/install.sh
@@ -4,5 +4,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp "$dir/ipaHelper" /usr/bin/
 sudo cp "$dir/ipaHelper.1" /usr/share/man/man1/
+cp "$dir/ipaHelper.qlgenerator" ~/Library/Quicklook
+cp "$dir/Resign.workflow" ~/Library/Services
 
 #exit 0
--- QuickLookPlugin/QuickLookPlugin/GenerateThumbnailForURL.m
+++ QuickLookPlugin/QuickLookPlugin/GenerateThumbnailForURL.m
@@ -5,7 +5,7 @@
 //#import <WebKit/WebKit.h>
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
-CGFloat drawArrayInRectInContext2(NSArray *array, CGRect rect, CGContextRef context, CGFloat lineSpacing, CGFloat fontSize);
+void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth, float ovalHeight);
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
 
 /* -----------------------------------------------------------------------------
@@ -74,6 +74,12 @@ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thum
         CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData ((__bridge CFDataRef)iconData);
         CGImageRef iconImage = CGImageCreateWithPNGDataProvider(imgDataProvider, NULL, true, kCGRenderingIntentDefault);
         
+        //Round the corners
+        CGContextBeginPath(context);
+        addRoundedRectToPath(context, iconRect, 5, 5);
+        CGContextClosePath(context);
+        CGContextClip(context);
+        
         CGContextDrawImage(context, iconRect, iconImage);
         
         CFRelease(iconImage);
@@ -87,121 +93,25 @@ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thum
     return noErr;
 }
 
-//OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
-//{
-//    NSURL *ipaURL = (__bridge NSURL *)url;
-//    
-//    NSTask *task = [[NSTask alloc] init];
-//    [task setLaunchPath:@"/usr/bin/ipaHelper"];
-//    [task setArguments:[NSArray arrayWithObjects:@"summary", [ipaURL path], @"--json", @"--ql", nil]];
-//    
-//    NSPipe *pipe = [NSPipe pipe];
-//    [task setStandardOutput: pipe];
-//    //The magic line that keeps your log where it belongs
-//    [task setStandardInput:[NSPipe pipe]];
-//    
-//    NSFileHandle *file;
-//    file = [pipe fileHandleForReading];
-//    
-//    NSLog(@"Launching Task");
-//    [task launch];
-//    
-//    NSData *data;
-//    data = [file readDataToEndOfFile];
-//    
-//    NSDictionary *summaryDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers  error:nil];
-//    
-//    [[summaryDictionary copy] enumerateKeysAndObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
-//        NSLog(@"key: %@, value:%@", key, obj);
-//    }];
-//    
-//    CGSize contextSize = CGSizeMake(100.0, 100.0);
-//    CGFloat margin = 1.0;
-//    
-//    CGContextRef context = QLThumbnailRequestCreateContext(thumbnail, contextSize, false, NULL);
-//    
-//    if (context) {
-//        
-//        NSArray *appInfo = @[@{@"key":@"Bundle ID:",@"value":(NSString *)summaryDictionary[@"CFBundleIdentifier"]},
-//                             @{@"key":@"Version:",@"value":(NSString *)summaryDictionary[@"ShortBundleVersion"]},
-//                             ];
-//        NSArray *profileInfo = @[@{@"key":@"Profile:",@"value":(NSString *)summaryDictionary[@"App ID Name"]},
-//                                 @{@"key":@"Certificate:",@"value":(NSString *)summaryDictionary[@"Certificate Name"]},
-//                                 @{@"key":@"Team Name:",@"value":(NSString *)summaryDictionary[@"Team Name"]},
-//                                 @{@"key":@"Profile Expires:",@"value":(NSString *)summaryDictionary[@"Expiration Date"]},
-//                                 ];
-//        
-//        NSMutableArray *combinedArrays = [NSMutableArray arrayWithArray:appInfo];
-////        [combinedArrays addObject:@{@"key":@"",@"value":@""}];
-//        [combinedArrays addObjectsFromArray:profileInfo];
-//        
-//        drawArrayInRectInContext2(combinedArrays, CGRectMake(margin, margin, contextSize.width - 2 * margin, contextSize.height - 2 * margin), context, 2.0, 8.0);
-//        
-//        QLThumbnailRequestFlushContext(thumbnail, context);
-//        NSLog(@"Did it work?");
-//        CFRelease(context);
-//    }
-//    
-//    return noErr;
-//}
-
-CGFloat drawArrayInRectInContext2(NSArray *array, CGRect rect, CGContextRef context, CGFloat lineSpacing, CGFloat fontSize)
+void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth, float ovalHeight)
 {
-    NSGraphicsContext *nsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:(void *)context flipped:NO];
-    [NSGraphicsContext setCurrentContext:nsContext];
-    
-    //    NSMutableParagraphStyle *rightStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-    //    [rightStyle setAlignment:NSRightTextAlignment];
-    
-    NSMutableParagraphStyle *leftStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-    [leftStyle setAlignment:NSLeftTextAlignment];
-    [leftStyle setLineBreakMode:NSLineBreakByWordWrapping];
-    [leftStyle setLineSpacing:0.0];
-    
-    
-    NSDictionary *keyFontAttributes = @{NSFontAttributeName: [NSFont systemFontOfSize:fontSize],
-                                        NSForegroundColorAttributeName: [NSColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0],
-                                        NSParagraphStyleAttributeName: leftStyle,
-                                        
-                                        };
-    
-    NSDictionary *valueFontAttributes = @{  NSFontAttributeName: [NSFont systemFontOfSize:fontSize],
-                                            NSForegroundColorAttributeName: [NSColor blackColor],
-                                            NSParagraphStyleAttributeName: leftStyle,
-                                            };
-    
-    __block CGFloat longestKey;
-    __block CGFloat keyHeight;
-    
-    NSArray *keyArray = [array.copy valueForKey:@"key"];
-    
-    [keyArray enumerateObjectsUsingBlock:^(NSString *key, NSUInteger idx, BOOL *stop) {
-        NSRect testRect = [key boundingRectWithSize:NSMakeSize(500.0, 100.0) options:NSStringDrawingUsesLineFragmentOrigin attributes:keyFontAttributes];
-        if (testRect.size.width > longestKey) {
-            longestKey = testRect.size.width;
-        }
-        if (testRect.size.height > keyHeight) {
-            keyHeight = testRect.size.height;
-        }
-    }];
-    
-    __block CGFloat currentY = rect.size.height + rect.origin.y;
-    CGFloat startingY = currentY;
-    CGFloat horizontalSpacing = 4.0;
-    CGFloat startingValuePosition = rect.origin.x + longestKey + horizontalSpacing;
-    CGFloat valueWidth = rect.size.width - longestKey - horizontalSpacing;
-    
-    [array.copy enumerateObjectsUsingBlock:^(NSDictionary *dict, NSUInteger idx, BOOL *stop) {
-        NSString *key = dict[@"key"];
-        NSString *value = dict[@"value"];
-        [key drawInRect:NSMakeRect(rect.origin.x, currentY - keyHeight, longestKey, keyHeight) withAttributes:keyFontAttributes];
-        NSRect valueRect = [value boundingRectWithSize:NSMakeSize(valueWidth, 500.0) options:NSStringDrawingUsesLineFragmentOrigin attributes:valueFontAttributes];
-        CGFloat valueTextHeight = valueRect.size.height;
-        [value drawInRect:NSMakeRect(startingValuePosition, currentY - valueTextHeight, valueWidth, valueTextHeight) withAttributes:valueFontAttributes];
-        currentY -= valueTextHeight + lineSpacing;
-    }];
-    
-    return startingY - currentY;
+	float fw, fh;
+	if (ovalWidth == 0 || ovalHeight == 0) {
+		CGContextAddRect(context, rect);
+		return;
+	}
+	CGContextSaveGState(context);
+	CGContextTranslateCTM (context, CGRectGetMinX(rect), CGRectGetMinY(rect));
+	CGContextScaleCTM (context, ovalWidth, ovalHeight);
+	fw = CGRectGetWidth (rect) / ovalWidth;
+	fh = CGRectGetHeight (rect) / ovalHeight;
+	CGContextMoveToPoint(context, fw, fh/2);
+	CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);
+	CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1);
+	CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1);
+	CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1);
+	CGContextClosePath(context);
+	CGContextRestoreGState(context);
 }
 
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail)
